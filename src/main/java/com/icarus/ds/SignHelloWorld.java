699ea224cf26c0803f8aab17fc2cda4942e9823e
Icarus <thanght4391@gmail.com>
2022-09-29 12:07:16 +0700

    Sign a document using a temporary file

--- src/main/java/com/icarus/ds/SignHelloWorld.java
+++ src/main/java/com/icarus/ds/SignHelloWorld.java
@@ -7,6 +7,7 @@ import com.itextpdf.text.pdf.PdfSignatureAppearance;
 import com.itextpdf.text.pdf.PdfStamper;
 import com.itextpdf.text.pdf.security.*;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -23,6 +24,26 @@ public class SignHelloWorld {
         PdfReader reader = new PdfReader(src);
         FileOutputStream os = new FileOutputStream(dest);
         PdfStamper stamper = PdfStamper.createSignature(reader, os, '\0');
+        // Creating the appearance
+        PdfSignatureAppearance appearance = stamper.getSignatureAppearance();
+        appearance.setReason(reason);
+        appearance.setLocation(location);
+        appearance.setVisibleSignature(new Rectangle(36, 748, 144, 780), 1, "sig");
+        // Creating the signature
+        ExternalDigest digest = new BouncyCastleDigest();
+        ExternalSignature signature = new PrivateKeySignature(pk, digestAlgorithm, provider);
+        MakeSignature.signDetached(
+                appearance, digest, signature, chain, null, null, null, 0, subfilter);
+    }
+
+    public void sign(
+            String src, String tmp, String dest, Certificate[] chain, PrivateKey pk, String digestAlgorithm,
+            String provider, MakeSignature.CryptoStandard subfilter, String reason, String location)
+            throws GeneralSecurityException, IOException, DocumentException {
+        // Creating the reader and the stamper
+        PdfReader reader = new PdfReader(src);
+        FileOutputStream os = new FileOutputStream(dest);
+        PdfStamper stamper = PdfStamper.createSignature(reader, os, '\0', new File(tmp));
         // Creating the appearance
         PdfSignatureAppearance appearance = stamper.getSignatureAppearance();
         appearance.setReason(reason);
