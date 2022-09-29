b0e2e9a90ae093f91b6697f34432842629afbffe
Icarus <thanght4391@gmail.com>
2022-09-29 11:39:32 +0700

    Start signing with iText

---
+++ src/main/java/com/icarus/ds/SignHelloWorld.java
@@ -0,0 +1,38 @@
+package com.icarus.ds;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfSignatureAppearance;
+import com.itextpdf.text.pdf.PdfStamper;
+import com.itextpdf.text.pdf.security.*;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.PrivateKey;
+import java.security.cert.Certificate;
+
+public class SignHelloWorld {
+
+    public void sign(
+            String src, String dest, Certificate[] chain, PrivateKey pk, String digestAlgorithm, String provider,
+            MakeSignature.CryptoStandard subfilter, String reason, String location)
+            throws GeneralSecurityException, IOException, DocumentException {
+        // Creating the reader and the stamper
+        PdfReader reader = new PdfReader(src);
+        FileOutputStream os = new FileOutputStream(dest);
+        PdfStamper stamper = PdfStamper.createSignature(reader, os, '\0');
+        // Creating the appearance
+        PdfSignatureAppearance appearance = stamper.getSignatureAppearance();
+        appearance.setReason(reason);
+        appearance.setLocation(location);
+        appearance.setVisibleSignature(new Rectangle(36, 748, 144, 780), 1, "sig");
+        // Creating the signature
+        ExternalDigest digest = new BouncyCastleDigest();
+        ExternalSignature signature = new PrivateKeySignature(pk, digestAlgorithm, provider);
+        MakeSignature.signDetached(
+                appearance, digest, signature, chain, null, null, null, 0, subfilter);
+    }
+
+}
